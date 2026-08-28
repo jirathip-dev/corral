7a1b3849634da0270bf7b8add8746c6cba0ac3e0
Guy <115384744+jirathip-k@users.noreply.github.com>
2026-08-28 15:48:16 +0700

    pages: restore egui wasm demo site files (73062b3, #215)

---
+++ corral-web_bg.wasm.d.ts
@@ -0,0 +1,34 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const __wbg_intounderlyingsink_free: (a: number, b: number) => void;
+export const intounderlyingsink_abort: (a: number, b: any) => any;
+export const intounderlyingsink_close: (a: number) => any;
+export const intounderlyingsink_write: (a: number, b: any) => any;
+export const __wbg_intounderlyingsource_free: (a: number, b: number) => void;
+export const intounderlyingsource_cancel: (a: number) => void;
+export const intounderlyingsource_pull: (a: number, b: any) => any;
+export const __wbg_intounderlyingbytesource_free: (a: number, b: number) => void;
+export const intounderlyingbytesource_autoAllocateChunkSize: (a: number) => number;
+export const intounderlyingbytesource_cancel: (a: number) => void;
+export const intounderlyingbytesource_pull: (a: number, b: any) => any;
+export const intounderlyingbytesource_start: (a: number, b: any) => void;
+export const intounderlyingbytesource_type: (a: number) => number;
+export const wasm_bindgen_13b433f95525a7fc___convert__closures_____invoke___wasm_bindgen_13b433f95525a7fc___JsValue__core_9b3796e30d99ddb7___result__Result_____wasm_bindgen_13b433f95525a7fc___JsError___true_: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen_13b433f95525a7fc___convert__closures_____invoke___js_sys_79528fe09b3020cf___Function_fn_wasm_bindgen_13b433f95525a7fc___JsValue_____wasm_bindgen_13b433f95525a7fc___sys__Undefined___js_sys_79528fe09b3020cf___Function_fn_wasm_bindgen_13b433f95525a7fc___JsValue_____wasm_bindgen_13b433f95525a7fc___sys__Undefined_______true_: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen_13b433f95525a7fc___convert__closures_____invoke___wasm_bindgen_13b433f95525a7fc___JsValue______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_13b433f95525a7fc___convert__closures_____invoke___js_sys_79528fe09b3020cf___Array______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_13b433f95525a7fc___convert__closures_____invoke___js_sys_79528fe09b3020cf___Array______true__3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_13b433f95525a7fc___convert__closures_____invoke___core_9b3796e30d99ddb7___result__Result_____wasm_bindgen_13b433f95525a7fc___JsValue___true_: (a: number, b: number) => [number, number];
+export const wasm_bindgen_13b433f95525a7fc___convert__closures_____invoke_______true_: (a: number, b: number) => void;
+export const wasm_bindgen_13b433f95525a7fc___convert__closures_____invoke_______true__1_: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
